dcd644e8db21fe7bd4ae3635c14bcbe1cc1d0362
Natalia <nandreev@student.42berlin.de>
2024-04-22 20:53:46 +0200

    wip: parsing map

--- read_map.c
+++ read_map.c
@@ -6,7 +6,27 @@
 /*   By: nandreev <nandreev@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:23:28 by nandreev          #+#    #+#             */
-/*   Updated: 2024/04/19 16:25:16 by nandreev         ###   ########.fr       */
+/*   Updated: 2024/04/22 20:53:11 by nandreev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
+int is_rectangular(char *map)
+{
+    
+}
+
+int check_map(char *map)
+{
+    int file;
+
+    file = open(map, O_RDWR);
+
+    if (!is_rectangular(map)) 
+    {
+        write(1, "Error\nMap is not rectangular\n", 29);
+        return (0);
+    }
+
+    return (0); // if map is wrong
+}
--- so_long.h
+++ so_long.h
@@ -1,28 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nandreev <nandreev@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/19 16:23:12 by nandreev          #+#    #+#             */
-/*   Updated: 2024/04/22 20:39:05 by nandreev         ###   ########.fr       */
+/*   Created: 2024/04/22 20:21:53 by nandreev          #+#    #+#             */
+/*   Updated: 2024/04/22 20:37:59 by nandreev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
-
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-	{
-		if (check_map(argv[1]) == 0)
-		{
-			write(1, "Error\nMap is wrong\n", 20);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-		write(1, "Programm accepts only 1 argument\n", 33);
-	exit(EXIT_SUCCESS);
-}
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
