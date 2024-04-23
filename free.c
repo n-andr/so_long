bfc21f249435d8a79a3a6baeae53d80f802858b0
n-andr <150958639+n-andr@users.noreply.github.com>
2024-04-23 23:10:17 +0200

    free map

---
+++ free.c
@@ -0,0 +1,15 @@
+#include "so_long.h"
+
+void	free_game(t_game_info *game)
+{
+	int	i;
+
+	i = 0;
+	while (game->map[i]) //need to free one more that is already null?
+	{
+		//printf("line %d : %s", i, game->map[i]);
+		free (game->map[i]);
+		i ++;
+	}
+	free (game->map);
+}
